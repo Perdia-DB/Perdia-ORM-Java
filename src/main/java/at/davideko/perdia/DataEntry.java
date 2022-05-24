322034696e6b01aac121fdedad1aae3995886f0e
Davideko <45367400+Davideko212@users.noreply.github.com>
2022-05-24 19:37:05 +0200

    Created WIP DataType Class

---
+++ src/main/java/at/davideko/perdia/DataEntry.java
@@ -0,0 +1,17 @@
+package at.davideko.perdia;
+
+enum DataType {
+    STRING,
+    INTEGER,
+    FLOAT
+}
+
+public class DataEntry {
+    DataType type = null;
+
+    public DataEntry(DataType type) {
+
+    }
+}
+
+
